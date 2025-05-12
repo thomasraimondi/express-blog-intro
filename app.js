267231f3eb12a699cc5b041c1397e2b673becae8
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-12 14:09:40 +0200

    add new route bacheca

--- app.js
+++ app.js
@@ -41,6 +41,42 @@ app.get("/", (req, res) => {
   console.log(`Risposta inviata: ${responseData}`);
 });
 
+app.get("/bacheca", (req, res) => {
+  const posts = [
+    {
+      title: "post 1",
+      content: "contenuto 1",
+      image: "/img/ciambellone.jpeg",
+      tags: ["tag1", "tag1", "tag1", "tag1"],
+    },
+    {
+      title: "post 2",
+      content: "contenuto 2",
+      image: "/img/cracker_barbabietola.jpeg",
+      tags: ["tag1", "tag1", "tag1", "tag1"],
+    },
+    {
+      title: "post 3",
+      content: "contenuto 3",
+      image: "/img/pane_fritto_dolce.jpeg",
+      tags: ["tag1", "tag1", "tag1", "tag1"],
+    },
+    {
+      title: "post 4",
+      content: "contenuto 4",
+      image: "/img/pasta_barbabietola.jpeg",
+      tags: ["tag1", "tag1", "tag1", "tag1"],
+    },
+    {
+      title: "post 5",
+      content: "contenuto 5",
+      image: "/img/torta_paesana.jpeg",
+      tags: ["tag1", "tag1", "tag1", "tag1"],
+    },
+  ];
+  res.json(posts);
+});
+
 app.listen(port, () => {
   console.log("Serve in ascolto");
 });
