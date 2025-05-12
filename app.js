d4d0531aa821b0919b96810e96bb57ba29851306
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-12 17:05:26 +0200

    add frontend

--- app.js
+++ app.js
@@ -8,31 +8,31 @@ const posts = [
   {
     title: "post 1",
     content: "contenuto 1",
-    image: "/img/ciambellone.jpeg",
+    image: `${appUrl}/img/ciambellone.jpeg`,
     tags: ["tag1", "tag1", "tag1", "tag1"],
   },
   {
     title: "post 2",
     content: "contenuto 2",
-    image: "/img/cracker_barbabietola.jpeg",
+    image: `${appUrl}/img/cracker_barbabietola.jpeg`,
     tags: ["tag1", "tag1", "tag1", "tag1"],
   },
   {
     title: "post 3",
     content: "contenuto 3",
-    image: "/img/pane_fritto_dolce.jpeg",
+    image: `${appUrl}/img/pane_fritto_dolce.jpeg`,
     tags: ["tag1", "tag1", "tag1", "tag1"],
   },
   {
     title: "post 4",
     content: "contenuto 4",
-    image: "/img/pasta_barbabietola.jpeg",
+    image: `${appUrl}/img/pasta_barbabietola.jpeg`,
     tags: ["tag1", "tag1", "tag1", "tag1"],
   },
   {
     title: "post 5",
     content: "contenuto 5",
-    image: "/img/torta_paesana.jpeg",
+    image: `${appUrl}/img/torta_paesana.jpeg`,
     tags: ["tag1", "tag1", "tag1", "tag1"],
   },
 ];
@@ -40,16 +40,21 @@ const posts = [
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log("richiesta ricevuta");
+  console.log("richiesta ricevuta Home");
   const responseData = "Server del mio blog";
+  res.header({ "Access-Control-Allow-Origin": "*" });
   res.status(200).send(responseData);
-  console.log(`Risposta inviata: ${responseData}`);
 });
 
 app.get("/bacheca", (req, res) => {
-  posts.length > 0
-    ? res.status(200).json({ posts: posts })
-    : res.status(204).json(posts);
+  console.log("richiesta ricevuta Bacheca");
+  if (posts.length > 0) {
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(200).json({ posts: posts });
+  } else {
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(204).json(posts);
+  }
 });
 
 app.listen(APP_PORT, () => {
